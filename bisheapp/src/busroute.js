38050ac7bb95e5dd84ef277c6f2c0ac043b454f9
1-UP <1584824430@qq.com>
2018-05-10 21:38:16 +0800

    app compeleted0.89

--- bisheapp/src/busroute.js
+++ bisheapp/src/busroute.js
@@ -1,6 +1,6 @@
 import React,{Component} from 'react';
-import {Text,View,Dimensions,Image } from 'react-native';
-	import { Actions } from 'react-native-router-flux';
+import {Text,View,Dimensions,Image,ScrollView,ListView,TouchableOpacity } from 'react-native';
+import { Actions } from 'react-native-router-flux';
 
 let scw=Dimensions.get('window').width;
 let sch=Dimensions.get('window').height;
@@ -9,13 +9,47 @@ let sch=Dimensions.get('window').height;
 
 class Busroute extends Component{
 
-		constructor(props) {
-			super(props);
-		
-			this.state = {
-				name:'123'
-			};
-		}
+	constructor(props) {
+		super(props);
+		const ds=new ListView.DataSource({
+			rowHasChanged:(r1,r2)=>r1!=r2});
+		this.state = {
+			name:'123',
+			dataSource:ds.cloneWithRows([{
+				name:'鄞州区',
+				price:'价格',
+				index:'位置1'
+
+
+			},
+			{
+				name:'海曙区',
+				price:'价格',
+				index:'位置1'	
+
+
+			},
+			{
+				name:'北仑区',
+				price:'价格',
+				index:'位置1'
+
+
+			},
+			{
+				name:'江东区',
+				price:'价格',
+				index:'位置1'
+
+
+			},
+			{
+				name:'镇海区',
+				price:'价格',
+				index:'位置1'
+			}])
+		};
+	}
 
 
 
@@ -26,19 +60,39 @@ class Busroute extends Component{
 	}
 
 
+	renderRow1(rowData: string,sectionID: number, rowID: number){
+			 
+		
+			return(
+				<TouchableOpacity activeOpacity={0.9} 	>
+				<View>
+				<Text>
+				{rowData.name}
+				</Text>
+				<Text>
+				{rowData.price}
+				</Text>
+				<Text>
+				{rowData.index}
+				</Text>
+				</View>
+				</TouchableOpacity>
 
+				);
+		}
 
 
 
 	render(){
 
 		return(
-			<View>
-			<Text>
-				Busroute
-			</Text>
-			</View>
-		);
+			<ScrollView>
+			<ListView
+				dataSource={this.state.dataSource}
+				renderRow={this.renderRow1}
+				/>
+			</ScrollView>
+			);
 	}
 }
 
